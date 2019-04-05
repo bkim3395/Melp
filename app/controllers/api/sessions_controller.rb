03ed6b11e2f23c937550e1fa462c9b979138ab5c
bkim3395 <bkim3395@gmail.com>
2019-04-04 20:46:03 -0400

    error fix

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,7 +2,6 @@ class Api::SessionsController < ApplicationController
 
     def create
         @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
-        debugger
         if @user.nil?
             render json: ['Invalid email or password.'], status: 422
         else
